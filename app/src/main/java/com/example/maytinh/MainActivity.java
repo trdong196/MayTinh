4637203b35524a7b219654939f513b52cb4f0ebf
Acer <trdong196@gmail.com>
2024-08-29 22:35:53 +0700

    commit lan 1

---
+++ app/src/main/java/com/example/maytinh/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.maytinh;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtX, txtY;
+    TextView txtResult;
+    Button btnPlus;
+    Button btnMinus, btnMultiply, btnDivide, btnPercent;
+    private void innitControl(){
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtResult = findViewById(R.id.txtResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnPercent = findViewById(R.id.btnPercent);
+        //Button cộng
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x + y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        //Button trừ
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x - y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        //Button nhân
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x * y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        //Button chia
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x / y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+        //Button phần trăm
+        btnPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                int result = x % y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        innitControl();
+    }
+}
